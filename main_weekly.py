edcd43bf34e73ab0a82b7e43ea202a428e5a91a1
satoki252595 <satoki252595@gmail.com>
2024-05-19 21:35:04 +0900

    v0.1

--- main_weekly.py
+++ main_weekly.py
@@ -3,4 +3,8 @@ from lib.stockOp import getStockCodes,getStockInfo
 
 #create TBL & code insert
 df = getStockCodes.getCreditBalancesInfo(1)
+df = getStockCodes.getCreditBalancesInfo(2)
+df = getStockCodes.getCreditBalancesInfo(3)
+df = getStockCodes.getCreditBalancesInfo(4)
+df = getStockCodes.getCreditBalancesInfo(5)
 insert.insertStockCreditBalances(df)
